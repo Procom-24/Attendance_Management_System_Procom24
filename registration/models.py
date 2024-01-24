465f806336bb6f32cc3489e11679f2b3e478a457
abbasghori29 <132843772+abbasghori29@users.noreply.github.com>
2024-01-24 20:53:01 +0500

    Database config+model changes+qr generation,saving and sending (#4)
    * csv file upload and feed to database
    
    * database config and qr generation,saving and send
    
    * changes in models
    
    ---------
    
    Co-authored-by: Syeda Samaha Batool Rizvi <94197721+samaha-rizvi-4578@users.noreply.github.com>

--- registration/models.py
+++ registration/models.py
@@ -8,11 +8,10 @@ class Participants(models.Model):
     lastname = models.CharField(max_length=255)
     email = models.EmailField()
     phonenumber = models.CharField(max_length=20)
-    # studentid = models.CharField(max_length=255)   procom main har student ki seperate uni id ka koi kaam nhi hoga, we only have concerns with particapnt id
     universityname = models.CharField(max_length=255)
     contestname = models.CharField(max_length=255)
-    age = models.PositiveIntegerField()
-    attendanceStatus = models.CharField(max_length=255)
+    age = models.CharField(max_length=3)
+    attendanceStatus = models.CharField(max_length=255,default='A')
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
@@ -29,41 +28,24 @@ class ParticipantCard(models.Model):
 class UserAccount(models.Model):
     userID = models.CharField(primary_key=True, max_length=255)
     username = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(null=True)
     passwordhash = models.CharField(max_length=255)
-  #  Participants_ParticipantID = models.ForeignKey(Participants, on_delete=models.CASCADE)
-
     def __str__(self):
         return f"User {self.username}"
 
 class QRcode(models.Model):
     QRcodeId = models.AutoField(primary_key=True)
-    QRcodetype = models.CharField(max_length=255)
-    OTP = models.CharField(max_length=255)
-    confirmationstatus = models.CharField(max_length=255)
+    DataQRcode1 = models.CharField(max_length=255)
+    DataQRcode2 = models.CharField(max_length=255)
+    image_qr1 = models.ImageField(upload_to='qrcodes/', null=True, blank=True)
+    image_qr2 = models.ImageField(upload_to='qrcodes/', null=True, blank=True)
+
+    # OTP = models.CharField(max_length=255)
+    # confirmationstatus = models.CharField(max_length=255)
     Participants_participantID = models.ForeignKey(Participants, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"QR Code {self.QRcodeId} - {self.Participants_participantID}"
-
-# merged attendance table in participants table and registration say hamara koi ta'aluq nhi so remove this
-# class Attendence(models.Model):
-#     AttendenceID = models.AutoField(primary_key=True)
-#     eventdate = models.DateField()
-#     status = models.CharField(max_length=255)
-#     Participants_participantID = models.ForeignKey(Participants, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return f"Attendance {self.AttendenceID} - {self.Participants_participantID}"
-
-# class Registrations(models.Model):
-#     RegistrationID = models.AutoField(primary_key=True)
-#     registrationdate = models.DateField()
-#     status = models.CharField(max_length=255)
-#     Participants_participantID = models.ForeignKey(Participants, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return f"Registration {self.RegistrationID} - {self.Participants_participantID}"
 
 class Certificates(models.Model):
     certificateID = models.CharField(primary_key=True, max_length=255)
--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import participant_list, send_qr, send_qr_all, home, update_attendance
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import participant_list, send_qr, send_qr_all, home, upload_csv, uploadPage, update_attendance
 
 urlpatterns = [
     path('', home, name='home'),  # URL pattern for the root path
     path('participants/', participant_list, name='participant_list'),
     path('send_qr/<int:participant_id>/<int:qr_type>/', send_qr, name='send_qr'),
     path('send_qr_all/<int:qr_type>/', send_qr_all, name='send_qr_all'),
+    path('uploadFile/', uploadPage, name='uploadPage'),
+    path('upload/', upload_csv, name='upload_csv'),
     path('update_attendance/<str:unique_identifier>/', update_attendance, name='update_attendance'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
