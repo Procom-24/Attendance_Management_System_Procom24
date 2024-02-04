186fd756d4f788dc66c81245865018ab24f16182
rizvi <k214578@nu.edu.pk>
2024-02-04 20:44:42 +0500

    changes aproved commiting directly to main

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import participant_list, send_qr, send_qr_all, home, upload_csv, uploadPage, update_attendance
+from .views import participant_list, send_qr, send_qr_all, home, upload_csv, uploadPage, update_attendance, login
 
 urlpatterns = [
-    path('', home, name='home'),  # URL pattern for the root path
+    # path('', home, name='home'),  # URL pattern for the root path
+    path('', login, name='login'),
     path('participants/', participant_list, name='participant_list'),
     path('send_qr/<int:participant_id>/<int:qr_type>/', send_qr, name='send_qr'),
     path('send_qr_all/<int:qr_type>/', send_qr_all, name='send_qr_all'),
