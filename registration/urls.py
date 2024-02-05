d50aaf61d42b5e3ffc0c08371abf6259dfeb20c7
Syed Fasih <elite9145build@gmail.com>
2024-02-06 03:55:12 +0500

    Updated Home.html
    => Updated urls.py to include 'home/'
    
    => Created home page + css

--- registration/urls.py
+++ registration/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from .views import participant_list, send_qr, send_qr_all, home, upload_csv, uploadPage, scan_qr, mark_attendance, login
 
 urlpatterns = [
-    # path('', home, name='home'),  # URL pattern for the root path
+    path('home/', home, name='home'),
     path('', login, name='login'),
     path('participants/', participant_list, name='participant_list'),
     path('send_qr/<int:participant_id>/<int:qr_type>/', send_qr, name='send_qr'),
